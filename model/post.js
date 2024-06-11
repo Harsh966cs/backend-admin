88d0f1f46ba7e637167420bbafaa1bdbe2ddbd61
Harsh966cs <120917444+Harsh966cs@users.noreply.github.com>
2024-06-11 11:15:43 +0530

    Update post.js of date

--- model/post.js
+++ model/post.js
@@ -2,15 +2,14 @@ import mongoose, { Schema } from "mongoose";
 
 const postSchema = new Schema({
   source: {
-    id: { type: Schema.Types.Mixed, default: null },
-    name: { type: String, required: [true, "Source name is required"] }
+    id: { type: Schema.Types.Mixed, default: null }
   },
   author: { type: String },
   title: { type: String, required: [true, "Title is required"] },
   description: { type: String, required: [true, "Description is required"] },
   url: { type: String, required: [true, "URL is required"], unique: [true, "URL must be unique"] },
   urlToImage: { type: String, required: [true, "URL to image is required"], unique: [true, "URL to image must be unique"] },
-  publishedAt: { type: Date, required: [true, "Published date is required"] },
+  publishedAt: { type: Date, default:Date.now  },
   content: { type: String }
 });
 
